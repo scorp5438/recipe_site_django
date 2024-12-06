6963f37ecd242024381401ce193b25a7fbac5563
ScOrP <alex_77_90@mail.ru>
2024-12-06 16:22:11 +0500

    upgrade project

--- recipe_site_django/recipe_site_django/settings.py
+++ recipe_site_django/recipe_site_django/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', 'django-insecure-^1)!8zg(fsqvagx--fdjns
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DJANGO_DEBUG', '0') == '1'
+# DEBUG = True
 
 ALLOWED_HOSTS = [
                     '0.0.0.0',
